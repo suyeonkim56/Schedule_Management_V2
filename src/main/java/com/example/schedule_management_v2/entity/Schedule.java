f5d65d843482ef9a8415a89d8a854b6b176f20a9
suyeonkim <suyeonk56@gmail.com>
2025-02-10 21:01:57 +0900

    board를 schedule로 리네이밍

--- src/main/java/com/example/schedule_management_v2/entity/Schedule.java
+++ src/main/java/com/example/schedule_management_v2/entity/Schedule.java
@@ -3,8 +3,8 @@ package com.example.schedule_management_v2.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "board")
-public class Board extends BaseEntity {
+@Table(name = "schedule")
+public class Schedule extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
